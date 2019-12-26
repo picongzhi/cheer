d117ad25d3ea7d51f86c83d74d64698356f3a15d
picongzhi <picongzhi@gmail.com>
2019-12-26 20:16:43 +0800

    feat(server): enable transaction management

--- cheer-server/cheer-server-api/src/main/java/com/pcz/cheer/Application.java
+++ cheer-server/cheer-server-api/src/main/java/com/pcz/cheer/Application.java
@@ -2,6 +2,7 @@ package com.pcz.cheer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tk.mybatis.spring.annotation.MapperScan;
 
 /**
@@ -9,6 +10,7 @@ import tk.mybatis.spring.annotation.MapperScan;
  */
 @SpringBootApplication
 @MapperScan(basePackages = {"com.pcz.cheer.mapper"})
+@EnableTransactionManagement
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
