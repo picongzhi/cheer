e6cd21fbc6fee6f385776313778e365ede9527a3
picongzhi <picongzhi@gmail.com>
2020-01-03 17:11:56 +0800

    feat(index): add home page layout

--- cheer-vue/src/views/Home.vue
+++ cheer-vue/src/views/Home.vue
@@ -1,17 +1,29 @@
 <template>
   <div class="home">
-    <markdown-editor v-model="content" height="300px"></markdown-editor>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="300px">Aside</el-aside>
+        <el-main>
+          <!-- <markdown-editor
+            v-model="content"
+            height="300px"
+          ></markdown-editor> -->
+        </el-main>
+      </el-container>
+    </el-container>
+
   </div>
 </template>
 
 <script>
 import { Message } from 'element-ui'
-import MarkdownEditor from '@/components/markdown'
+// import MarkdownEditor from '@/components/markdown'
 import { getUserInfo } from '@/api/user'
 
 export default {
   name: 'home',
-  components: { MarkdownEditor },
+  // components: { MarkdownEditor },
   data () {
     return {
       content: ''
@@ -28,3 +40,36 @@ export default {
   }
 }
 </script>
+
+<style lang="stylus" scoped>
+.home {
+  height: 100%;
+
+  .el-container {
+    height: 100%;
+
+    .el-header, .el-footer {
+      background-color: #409EFF;
+      color: #333;
+      text-align: center;
+      line-height: 60px;
+    }
+
+    .el-container {
+      .el-main {
+        background-color: #FDE2E2;
+        color: #333;
+        text-align: center;
+        line-height: 200px;
+      }
+
+      .el-aside {
+        background-color: #E1F3D8;
+        color: #333;
+        text-align: center;
+        line-height: 200px;
+      }
+    }
+  }
+}
+</style>
